a767bb566e803aaff9c6cf7a3ed0032c45260f65
mmiask <mmiask92@gmail.com>
2024-05-30 12:16:52 +0200

    add 'remove from favorites' feature and style the page; used custom Anime component

--- client/src/routes/[anime_id]/+page.svelte
+++ client/src/routes/[anime_id]/+page.svelte
@@ -1,32 +1,62 @@
 <script lang="ts">
     import { enhance } from "$app/forms";
-    import Anime from "../anime.svelte";
     import type { PageData } from "./$types";
 
     export let data: PageData;
 </script>
 
-<Anime
-    title={data.anime.title}
-    mal_id={data.anime.mal_id}
-    image={data.anime.images.webp.image_url}
-/>
+<div class="h-screen">
+    <div class="my-5 ml-5">
+        <a
+            class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            href="/"
+        >
+            Go back to list
+        </a>
+    </div>
 
-<p>{data.anime.synopsis}</p>
+    <div class="mx-auto flex flex-col gap-y-5 px-4 md:w-1/2 md:px-0">
+        <div class="flex flex-col items-center gap-5">
+            <h1 class="text-4xl">
+                {data.anime.title}
+            </h1>
 
-<form action="?/addToFavorites" method="post" use:enhance>
-    <input type="hidden" name="mal_id" value={data.anime.mal_id} />
-    <input type="hidden" name="title" value={data.anime.title} />
-    <input
-        type="hidden"
-        name="image"
-        value={data.anime.images.webp.image_url}
-    />
-    <button class="bg-gray-300 rounded p-4 mt-4" type="submit">
-        Add to favorites
-    </button>
-</form>
+            <img
+                src={data.anime.images.webp.image_url}
+                alt={data.anime.title}
+            />
+        </div>
 
-<div class="mt-10">
-    <a class="bg-gray-300 rounded p-4" href="/">Go back to list</a>
+        <p class="text-justify text-base leading-loose">
+            {data.anime.synopsis}
+        </p>
+
+        <div class="flex flex-row justify-center gap-4 pb-4">
+            <form action="?/addToFavorites" method="post" use:enhance>
+                <input type="hidden" name="mal_id" value={data.anime.mal_id} />
+                <input type="hidden" name="title" value={data.anime.title} />
+                <input
+                    type="hidden"
+                    name="image"
+                    value={data.anime.images.webp.image_url}
+                />
+                <button
+                    class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                    type="submit"
+                >
+                    Add to favorites
+                </button>
+            </form>
+
+            <form action="?/removeFromFavorites" method="post" use:enhance>
+                <input type="hidden" name="mal_id" value={data.anime.mal_id} />
+                <button
+                    class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                    type="submit"
+                >
+                    Remove From Favorites
+                </button>
+            </form>
+        </div>
+    </div>
 </div>
